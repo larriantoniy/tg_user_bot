e69cb9e095a33ed7a9e2b114cbef02ac567a1646
larrianton <larrianton@gmail.com>
2025-06-25 17:05:53 +0400

    add additional processing

--- internal/adapters/tdlib/tdlib.go
+++ internal/adapters/tdlib/tdlib.go
@@ -1,11 +1,11 @@
 package tdlib
 
 import (
-	"log/slog"
-
 	"github.com/larriantoniy/tg_user_bot/internal/domain"
 	"github.com/larriantoniy/tg_user_bot/internal/ports"
 	"github.com/zelenin/go-tdlib/client"
+	"log/slog"
+	"strings"
 )
 
 // TDLibClient реализует ports.TelegramClient через go-tdlib
@@ -79,22 +79,28 @@ func (t *TDLibClient) JoinChannel(username string) error {
 	return nil
 }
 func (t *TDLibClient) JoinChannels(chs []string) {
+	joinedChs, err := t.GetJoinedChannels()
+	if err != nil {
+		t.logger.Error("Failed to receive joined channels", "stop Joining channels")
+		return
+	}
 	for _, ch := range chs {
-		isMember, err := t.IsChannelMember(ch)
-		if err != nil {
-			t.logger.Error("Failed to check membership", "channel", ch, "error", err)
-			continue
-		}
-		if isMember {
-			t.logger.Info("Already a member, skipping join", "channel", ch)
-			continue
-		}
-
-		if err := t.JoinChannel(ch); err != nil {
-			// Если ошибка содержит USER_ALREADY_PARTICIPANT — просто логируем и продолжаем
-			t.logger.Error("Failed to join channel", "channel", ch, "error", err)
+		_, ok := joinedChs[ch]
+		if !ok && strings.Contains(ch, "@") {
+			if err := t.JoinChannel(ch); err != nil {
+				// Если ошибка содержит USER_ALREADY_PARTICIPANT — просто логируем и продолжаем
+				t.logger.Error("Failed to join channel", "channel", ch, "error", err)
+			} else {
+				t.logger.Info("Successfully joined channel", "channel", ch)
+			}
 		} else {
-			t.logger.Info("Successfully joined channel", "channel", ch)
+			if _, err := t.client.JoinChatByInviteLink(&client.JoinChatByInviteLinkRequest{
+				InviteLink: ch,
+			}); err != nil {
+				t.logger.Error("Failed to join channel", "channel", ch, "error", err)
+			} else {
+				t.logger.Info("Successfully joined channel", "channel", ch)
+			}
 		}
 	}
 
@@ -109,16 +115,14 @@ func (t *TDLibClient) Listen() (<-chan domain.Message, error) {
 	go func() {
 		defer close(out)
 		for update := range listener.Updates {
-			// Фильтруем только новые сообщения
+			t.logger.Debug("Received new message")
 			if upd, ok := update.(*client.UpdateNewMessage); ok {
-				if textMsg, ok := upd.Message.Content.(*client.MessageText); ok {
-					out <- domain.Message{
-						ChatID: upd.Message.ChatId,
-						Text:   textMsg.Text.Text,
-					}
-					t.logger.Debug("Dispatched message", "chat_id", upd.Message.ChatId)
+				_, err := t.processUpdateNewMessage(out, upd)
+				if err != nil {
+					t.logger.Error("Error process UpdateNewMessage msg content type", upd.Message.Content.MessageContentType())
 				}
 			}
+			t.logger.Debug("Skipping new message is not UpdateNewMessage Type")
 		}
 	}()
 
@@ -155,4 +159,104 @@ func (t *TDLibClient) IsChannelMember(username string) (bool, error) {
 		t.logger.Debug("Bot not member", "chat_id", chat.Id)
 		return false, nil
 	}
+}
+
+func (t *TDLibClient) GetJoinedChannels() (map[string]bool, error) {
+	var (
+		// для первой страницы используем максимально возможный order
+		//@todo увеличить лимит через pagination
+		limit int32 = 200 // TDLib рекомендует не запрашивать >100–200 за раз
+	)
+	channels := make(map[string]bool)
+
+	for {
+		// 1) Получаем страницу чатов из основного списка (ChatListMain)
+		resp, err := t.client.GetChats(&client.GetChatsRequest{
+			ChatList: &client.ChatListMain{},
+			Limit:    limit,
+		})
+		if err != nil {
+			t.logger.Error("GetChats failed", "error", err)
+			return nil, err
+		}
+		if len(resp.ChatIds) == 0 {
+			// больше страниц нет
+			break
+		}
+
+		// 2) Для каждого chatID запрашиваем полные данные и отбираем только каналы
+		for _, chatID := range resp.ChatIds {
+			chat, err := t.client.GetChat(&client.GetChatRequest{ChatId: chatID})
+			if err != nil {
+				t.logger.Warn("GetChat failed, skipping", "chat_id", chatID, "chat title:", chat.Title, "error", err)
+				continue
+			}
+			//Супергруппа (тип ChatTypeSupergroup с IsChannel=false) по умолчанию приватная, но может быть сделана публичной путём назначения username
+			//medium.com
+			//core.telegram.org
+			//Проверка приватности сводится к получению объекта Supergroup и проверке поля Username: если строка пуста — группа приватная, иначе — публичная
+			if sg, ok := chat.Type.(*client.ChatTypeSupergroup); ok && sg.IsChannel {
+				channels[chat.Title] = true
+
+			}
+		}
+	}
+
+	return channels, nil
+}
+
+func (t *TDLibClient) getChatTitle(chatID int64) (string, error) {
+
+	chat, err := t.client.GetChat(&client.GetChatRequest{
+		ChatId: chatID,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return chat.Title, nil
+}
+
+func (t *TDLibClient) processUpdateNewMessage(out chan domain.Message, upd *client.UpdateNewMessage) (<-chan domain.Message, error) {
+	chatName, err := t.getChatTitle(upd.Message.ChatId)
+	if err != nil {
+		t.logger.Info("Error getting chat title", err)
+		chatName = ""
+	}
+	switch content := upd.Message.Content.(type) {
+	case *client.MessageText:
+		return t.processMessageText(out, content, upd.Message.ChatId, chatName)
+	case *client.MessagePhoto:
+		return t.processMessagePhoto(out, content, upd.Message.ChatId, chatName)
+	default:
+		t.logger.Debug("cant switch type update")
+		return out, nil
+	}
+}
+func (t *TDLibClient) processMessagePhoto(out chan domain.Message, msg *client.MessagePhoto, msgChatId int64, ChatName string) (<-chan domain.Message, error) {
+	var text string
+	photoFileIDs := make([]string, 0)
+	for _, size := range msg.Photo.Sizes {
+		if size.Photo.Remote != nil {
+			photoFileIDs = append(photoFileIDs, size.Photo.Remote.Id)
+		}
+	}
+	if msg.Caption != nil {
+		text = msg.Caption.Text
+	}
+	out <- domain.Message{
+		ChatID:       msgChatId,
+		Text:         text,
+		ChatName:     ChatName,
+		PhotoFileIds: photoFileIDs,
+	}
+	return out, nil
+}
+func (t *TDLibClient) processMessageText(out chan domain.Message, msg *client.MessageText, msgChatId int64, ChatName string) (<-chan domain.Message, error) {
+	out <- domain.Message{
+		ChatID:   msgChatId,
+		Text:     msg.Text.Text,
+		ChatName: ChatName,
+	}
+	return out, nil
 }
