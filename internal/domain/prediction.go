c8075249caac9bc62b7615bb2c68fef93ed2d41d
larrianton <larrianton@gmail.com>
2025-06-27 13:42:05 +0400

    change predictio type

--- internal/domain/prediction.go
+++ internal/domain/prediction.go
@@ -6,5 +6,7 @@ type Prediction struct {
 	ID        string    // UUID,  часть ключа в Redis
 	ChatID    int64     // откуда пришло сообщение
 	RawText   string    // полный текст сообщения
+	ChatName  string    // ссылка или имя чата
 	CreatedAt time.Time // время парсинга
+	Sport     string    // вид спорта
 }
