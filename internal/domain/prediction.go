28c4ef13c1a97606561ab8820d565ddfec36372a
larrianton <larrianton@gmail.com>
2025-06-27 22:38:24 +0400

    upd logs

--- internal/domain/prediction.go
+++ internal/domain/prediction.go
@@ -3,10 +3,10 @@ package domain
 import "time"
 
 type Prediction struct {
-	ID        string    // UUID,  часть ключа в Redis
-	ChatID    int64     // откуда пришло сообщение
-	RawText   string    // полный текст сообщения
-	ChatName  string    // ссылка или имя чата
-	CreatedAt time.Time // время парсинга
-	Sport     string    // вид спорта
+	ID        string    `json:"id"`        // UUID,  часть ключа в Redis
+	ChatID    int64     `json:"chatId"`    // откуда пришло сообщение
+	RawText   string    `json:"text"`      // полный текст сообщения
+	ChatName  string    `json:"chatName"`  // ссылка или имя чата
+	CreatedAt time.Time `json:"createdAt"` // время парсинга
+	Sport     string    `json:"sport"`     // вид спорта
 }
