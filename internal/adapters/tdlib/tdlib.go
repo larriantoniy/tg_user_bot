a64c2b3715ef225646f5c2e064718f4ecc27b100
larrianton <larrianton@gmail.com>
2025-07-30 10:14:34 +0400

    up log level

--- internal/adapters/tdlib/tdlib.go
+++ internal/adapters/tdlib/tdlib.go
@@ -38,7 +38,7 @@ func NewClient(apiID int32, apiHash string, logger *slog.Logger) (ports.Telegram
 		FilesDirectory:     "./tdlib-files",
 	}
 	if _, err := client.SetLogVerbosityLevel(&client.SetLogVerbosityLevelRequest{
-		NewVerbosityLevel: 1,
+		NewVerbosityLevel: 4,
 	}); err != nil {
 		logger.Error("TDLib SetLogVerbosity level", "error", err)
 	}
